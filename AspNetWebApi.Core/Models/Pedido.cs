6dc0ed769520fa637839a2475c406af6109dae5a
Wellington Antonio Zaneze <wellingtonantonio_zaneze@live.com>
2018-02-23 01:36:47 -0300

    Models added and EF installed

---
+++ AspNetWebApi.Core/Models/Pedido.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetWebApi.Core.Models
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public int IdCliente { get; set; }
+
+    }
+}
---
+++ AspNetWebApi.Core/Models/Venda.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetWebApi.Core.Models
+{
+    public class Venda
+    {
+        public int IdPedido { get; set; }
+        public int IdProduto { get; set; }
+        public int Unid { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
